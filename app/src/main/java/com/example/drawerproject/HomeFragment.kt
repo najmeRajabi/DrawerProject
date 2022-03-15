56354e50695bfe3a09f8f40a403dd1098a8d491a
niha8515 <niha8515@yahoo.com>
2022-03-15 16:48:02 +0330

    set binding

--- app/src/main/java/com/example/drawerproject/HomeFragment.kt
+++ app/src/main/java/com/example/drawerproject/HomeFragment.kt
@@ -1,13 +1,16 @@
 package com.example.drawerproject
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.drawerproject.databinding.FragmentHomeBinding
+
 
 class HomeFragment : Fragment() {
 
+    lateinit var binding: FragmentHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +21,10 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
+
 
 
 }
