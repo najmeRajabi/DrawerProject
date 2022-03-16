67d1a70f005fc52475e680df2561f1429f102107
niha8515 <niha8515@yahoo.com>
2022-03-16 20:11:33 +0330

    show in fo in DetailFragment.kt

--- app/src/main/java/com/example/drawerproject/DetailFragment.kt
+++ app/src/main/java/com/example/drawerproject/DetailFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.drawerproject.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
 
+    val infoViewModel: InfoViewModel by viewModels()
+    lateinit var binding : FragmentDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +22,17 @@ class DetailFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        binding = FragmentDetailBinding.inflate(layoutInflater, container , false)
+
+        val index = requireArguments().getInt("index")
+
+        binding.txvNameDetail.text = infoViewModel.arrayInfo[index].name
+        binding.txvExplainDetail.text = infoViewModel.arrayInfo[index].explain
+
+
+
+        return binding .root
     }
+
 
 }
