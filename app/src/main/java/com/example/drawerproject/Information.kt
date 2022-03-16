9f958c9f0dd15761d0c13def47e63f8d2c2ec944
niha8515 <niha8515@yahoo.com>
2022-03-16 19:36:20 +0330

    add data class Information.kt

---
+++ app/src/main/java/com/example/drawerproject/Information.kt
@@ -0,0 +1,13 @@
+package com.example.drawerproject
+
+class Information {
+    val dataArray= arrayListOf(
+        Data("vfgvgvd","fgdgddxgv","https://source.unsplash.com/random"),
+        Data("aaaaa","aaaaaaaaaaaaaaaaa","https://source.unsplash.com/random"),
+        Data("bbbbbbb","bnmbvlkfvhju","https://source.unsplash.com/random"),
+        Data("kjf","jighjfgvb","https://source.unsplash.com/random"),
+        Data("dfdff","sdscsc","https://source.unsplash.com/random"),
+        Data("dfdf","dfddccsc","https://source.unsplash.com/random")
+    )
+}
+data class Data(val name: String , val explain: String , val image : String)
