00aed0d643f16bdeeee4d1a692835b551d7a0e58
niha8515 <niha8515@yahoo.com>
2022-03-17 01:31:39 +0330

    add setOnClick register SettingFragment.kt

--- app/src/main/java/com/example/drawerproject/SettingFragment.kt
+++ app/src/main/java/com/example/drawerproject/SettingFragment.kt
@@ -1,13 +1,24 @@
 package com.example.drawerproject
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import com.example.drawerproject.databinding.FragmentSettingBinding
+
+var NUMBER= 4
+var NAME = "name"
+var NATIONAL = "nationalCode"
+var PHONE = "phone"
+var IMAGE = "image"
 
 class SettingFragment : Fragment() {
 
+    lateinit var binding: FragmentSettingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,8 +29,111 @@ class SettingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentSettingBinding.inflate(layoutInflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        binding.radioGroupTheme.setOnCheckedChangeListener{}
+        checkRegistered()
+
+        binding.btnRegister.setOnClickListener {
+            if (check()){
+                saveInSheardPref()
+                changeVisibilityRegister()
+                showInTxv()
+            }
+        }
+        binding.btnChangeInfo.setOnClickListener {
+            changeVisibility()
+        }
+
+        binding.txvSeekbar.text= NUMBER.toString()
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                if (p0 != null) {
+                    NUMBER = p0.progress
+                    binding.txvSeekbar.text= NUMBER.toString()
+                }
+            }
+        })
+    }
+
+    private fun checkRegistered() {
+        val sharedPreferences:SharedPreferences =
+            requireActivity().getSharedPreferences("HW10" , Context.MODE_PRIVATE)
+        if (!sharedPreferences.getString(NAME,"").isNullOrBlank()){
+            changeVisibilityRegister()
+            showInTxv()
+        }
+    }
+
+    private fun changeVisibility() {
+        binding.llEdt.visibility= View.VISIBLE
+        binding.llTxv.visibility= View.GONE
+    }
+
+
+    private fun showInTxv() {
+
+        val sharedPreferences:SharedPreferences =
+            requireActivity().getSharedPreferences("HW10" , Context.MODE_PRIVATE)
+        sharedPreferences.apply {
+            binding.txvNameS.text=getString(NAME ,"")
+            binding.txvNationalCodeS.text= getString(NATIONAL ,"")
+            binding.txvPhoneS.text=getString(PHONE ,"")
+        }
+
+    }
+
+    private fun changeVisibilityRegister() {
+        binding.llEdt.visibility= View.GONE
+        binding.llTxv.visibility= View.VISIBLE
+    }
+
+    private fun saveInSheardPref() {
+        val sharedPreferences:SharedPreferences =
+            requireActivity().getSharedPreferences("HW10" , Context.MODE_PRIVATE)
+        sharedPreferences.edit().apply {
+            clear()
+            putString(NAME ,binding.edtName.text.toString())
+            putString(NATIONAL ,binding.edtNationalCode.text.toString())
+            putString(PHONE ,binding.edtPhone.text.toString())
+            putString(IMAGE ,binding.edtPhoto.text.toString())
+            apply()
+        }
+    }
+
+    private fun check(): Boolean {
+        var  result = false
+        when {
+            binding.edtName.text.isNullOrBlank() -> {
+                binding.edtName.error = "empty!"
+            }
+            binding.edtNationalCode.text.isNullOrBlank() -> {
+                binding.edtNationalCode.error = "empty!"
+            }
+            binding.edtPhone.text.isNullOrBlank() -> {
+                binding.edtPhone.error = "empty!"
+            }
+            binding.edtPhoto.text.isNullOrBlank() -> {
+                binding.edtPhoto.error = "empty!"
+            }
+            else -> result= true
+        }
+        return result
     }
 
 
