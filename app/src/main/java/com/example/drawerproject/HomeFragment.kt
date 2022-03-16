ca657c34cdcd5f84a28e40dbe6df47a646458a6a
niha8515 <niha8515@yahoo.com>
2022-03-16 20:10:54 +0330

    set on click items

--- app/src/main/java/com/example/drawerproject/HomeFragment.kt
+++ app/src/main/java/com/example/drawerproject/HomeFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.drawerproject.databinding.FragmentHomeBinding
 
 
@@ -25,17 +27,32 @@ class HomeFragment : Fragment() {
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
-        val arrayBox= arrayListOf(binding.includeImg1,
-        binding.includeImg2, binding.includeImg3,
-        binding.includeImg4, binding.includeImg5,
-            binding.includeImg6)
 
-        val itemList = infoViewModel.arrayInfo
 
 
         return binding.root
     }
 
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val arrayBox= arrayListOf(binding.includeImg1,
+            binding.includeImg2, binding.includeImg3,
+            binding.includeImg4, binding.includeImg5,
+            binding.includeImg6)
+
+        val itemList = infoViewModel.arrayInfo
+
+        for (i in 0..5){
+            arrayBox[i].imvHome.setOnClickListener {
+                val bundle = bundleOf("index" to i)
+                findNavController().navigate(R.id.action_homeFragment_to_detailFragment , bundle)
+            }
+            arrayBox[i].textView.text = itemList[i].name
+
+        }
+
+    }
 
 }
