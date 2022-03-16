6e81d9ec45608039437d0ab24cb7e254ca483842
niha8515 <niha8515@yahoo.com>
2022-03-16 21:26:29 +0330

    add themes.xml

--- app/src/main/java/com/example/drawerproject/DetailFragment.kt
+++ app/src/main/java/com/example/drawerproject/DetailFragment.kt
@@ -1,11 +1,15 @@
 package com.example.drawerproject
 
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.RecyclerView
 import com.example.drawerproject.databinding.FragmentDetailBinding
 
 class DetailFragment : Fragment() {
@@ -34,5 +38,17 @@ class DetailFragment : Fragment() {
         return binding .root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.imvIcon.setOnClickListener {
+            Toast.makeText(activity,"share" , Toast.LENGTH_SHORT).show()
+        }
+    }
+
+//    private fun share() {
+//        val intent = Intent(activity, Intent.ACTION_SEND)
+//        startActivity(intent)
+//    }
 
 }
