296b88733a44ce9be84d02fcde710e2b359fb9e5
niha8515 <niha8515@yahoo.com>
2022-03-28 02:02:09 +0430

    add binding

--- app/src/main/java/com/example/drawerproject/AccountFragment.kt
+++ app/src/main/java/com/example/drawerproject/AccountFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.drawerproject.databinding.FragmentAccountBinding
 
 
 class AccountFragment : Fragment() {
 
+    lateinit var binding: FragmentAccountBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +21,8 @@ class AccountFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        binding = FragmentAccountBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
 }
