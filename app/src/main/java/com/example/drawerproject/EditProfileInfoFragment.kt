607543d0a9b01379240b11ec608d88af023472b1
niha8515 <niha8515@yahoo.com>
2022-03-28 01:39:30 +0430

    add onclick to save info from EditProfileInfoFragment.kt

--- app/src/main/java/com/example/drawerproject/EditProfileInfoFragment.kt
+++ app/src/main/java/com/example/drawerproject/EditProfileInfoFragment.kt
@@ -1,13 +1,18 @@
 package com.example.drawerproject
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.drawerproject.databinding.FragmentEditProfileInfoBinding
 
 class EditProfileInfoFragment : Fragment() {
 
+    lateinit var binding: FragmentEditProfileInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +22,52 @@ class EditProfileInfoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile_info, container, false)
+        binding = FragmentEditProfileInfoBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnRegister.setOnClickListener {
+            if (check()){
+                saveInSharedPref()
+                Toast.makeText(activity, "registered" , Toast.LENGTH_SHORT).show()
+                activity?.onBackPressed()
+            }
+        }
+    }
+    private fun check(): Boolean {
+        var  result = false
+        when {
+            binding.edtName.text.isNullOrBlank() -> {
+                binding.edtName.error = "empty!"
+            }
+            binding.edtNationalCode.text.isNullOrBlank() -> {
+                binding.edtNationalCode.error = "empty!"
+            }
+            binding.edtPhone.text.isNullOrBlank() -> {
+                binding.edtPhone.error = "empty!"
+            }
+            binding.edtEmail.text.isNullOrBlank() -> {
+                binding.edtEmail.error = "empty!"
+            }
+            else -> result= true
+        }
+        return result
+    }
+
+    private fun saveInSharedPref() {
+        val sharedPreferences: SharedPreferences =
+            requireActivity().getSharedPreferences("HW10" , Context.MODE_PRIVATE)
+        sharedPreferences.edit().apply {
+            clear()
+            putString(NAME ,binding.edtName.text.toString())
+            putString(NATIONAL ,binding.edtNationalCode.text.toString())
+            putString(PHONE ,binding.edtPhone.text.toString())
+            putString(IMAGE ,binding.edtEmail.text.toString())
+            apply()
+        }
     }
 
 
